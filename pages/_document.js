a3759c27b1025a53a19b6bb963c55943d0a59f01
RealLanta <lanta1sme@gmail.com>
2022-09-07 16:31:45 +0800

    更新网站内容

--- pages/_document.js
+++ pages/_document.js
@@ -17,8 +17,8 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@reallanta1sme" />-->
-        <meta name="twitter:title" content={meta.title} />-->
+        <meta name="twitter:site" content="@reallanta1sme" />
+        <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
       </Head>
