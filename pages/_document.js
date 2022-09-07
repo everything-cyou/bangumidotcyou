a859bcc2d36793c81ac2644b5d5d6b8725dc6a75
RealLanta <lanta1sme@gmail.com>
2022-09-07 16:13:55 +0800

    更改网站信息以及添加图片

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Bangumi.cyou',
+    description: 'Bangumi.cyou 是非 Bangumi 官方的公益免费子域名',
+    image: 'https://www.bangumi.cyou/_next/image?url=%2Fimages%2F10586_crt_NL3Z3.jpeg'
   }
 
   return (
--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Bangumi.cyou',
+    site_url: 'https://bangumi.cyou',
+    feed_url: 'https://bangumi.cyou/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
